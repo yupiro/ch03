ae9d32fcdb196d06e7b4104f0b520cec00fc2767
yupiro_note <yupiro1983@gmail.com>
2016-07-21 22:21:27 +0900

    3rd commite

--- 03-getall/getall.js
+++ 03-getall/getall.js
@@ -5,3 +5,24 @@ var request = require('request');
 var URL = require('url');
 var fa = require('fs');
 var path = require('path');
+
+// ---共通の設定---
+// 階層の指定
+var LINK_LEVEL = 3;
+// 基準となるページURL
+var TARGET_URL = "http://nodejs.jp/nodejs.org_ja/docs/v0.10/api/";
+var list = {};
+
+// メイン処理
+downloadRec(TARGET_URL,0);
+
+// 指定のurlを最大levelまでダウンロード
+function downloadRec(url, level) {
+    // 最大レベルチェック
+    if (level >= LINK_LEVEL) return;
+    // 既出のサイトは無視する
+    if(list[url]) return;
+    list[url] = true;
+    // 基準ページ以外なら無視する
+
+}
