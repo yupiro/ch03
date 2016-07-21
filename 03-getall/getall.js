9ebb4463fdef2d023e32519fba7f8ea691924a80
yupiro_note <yupiro1983@gmail.com>
2016-07-21 22:47:39 +0900

    4th commite

--- 03-getall/getall.js
+++ 03-getall/getall.js
@@ -24,5 +24,28 @@ function downloadRec(url, level) {
     if(list[url]) return;
     list[url] = true;
     // 基準ページ以外なら無視する
+    var us = TARGET_URL.split("/");
+    us.pop();
+    var base = us.join("/");
+    if (url.indexOf(base) < 0) return;
+    // HTMLを取得する
+    client.fetch(url, {}, function(err, $, res){
+        // リンクされているページを取得
+        $("a").each(function (idx) {
+            // <a>タグのリンク先を辿る
+            var href = $(this).attr('href');
+            if (!href) return;
+            // 相対パスを絶対パスにに変換
+            href = URL.resolve(url, href);
+            // '#'以降を無視する(a.html#aa と a.html#bb は同じもの)
+            href = href.replace(/\#.+$/, ""); //末尾の'#'を消す
+            downloadRec(href, level + 1);
+        })
+        // ページを保存(ファイル名を決定する)
+        if (url.substr(url.length-1,1) == '/'){
+            url += "index.html"; //インデックスを自動追加
+        }
+
+    })
 
 }
